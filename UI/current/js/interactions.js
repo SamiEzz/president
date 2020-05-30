59075eb9df9627d548dae28baad6f1a1a894ce46
Sami_aZeus <sami.ezzerouali@hotmail.com>
2020-05-30 02:32:36 +0200

    interactions

--- UI/current/js/interactions.js
+++ UI/current/js/interactions.js
@@ -1,8 +1,71 @@
 const onglets = document.querySelectorAll('.nav-item');
 
+function getMyStackHTML(){
+    var numberOfCard = 5;
+    var cards = ["2.ca","2.co","2.tr","jo","5.co"];
+    var htmlHead= "<nav class='nav nav-pills nav-justified'>";
+    var htmlFooter= "</nav><h5 class='card-title'>Select cards</h5>   <p class='card-text'></p>   <a href='#' class='btn btn-primary'>Throw</a>";
+    var htmlBody= "";
+    var html = "";
+    html=html.concat(htmlHead);
+    for (let index = 0; index < cards.length; index++) {
+        const element = cards[index];
+        var dotindex = element.indexOf(".");
+        var number = element.slice(0,dotindex);
+        var symbol = element.slice(dotindex+1,element.length); 
+        console.log("symbole : "+symbol);
+        console.log("element : "+element);
+        console.log("dotindex : "+dotindex);
+        
+        htmlBody=htmlBody.concat("<a class='nav-item nav-link' href='#'>");
+        htmlBody=htmlBody.concat(number);
+        htmlBody=htmlBody.concat("<br><img style='width:150px' src='img/cards/");
+        htmlBody=htmlBody.concat(symbol);
+        htmlBody=htmlBody.concat(".png'></a>");
+        
+    }
+    html=html.concat(htmlBody);
+    html=html.concat(htmlFooter);
+    console.log(html);
+    return html;
+}
+
+function getHistoryOfActionsHTML(){
+    var tableStack = [["9.ca","9.co","9.tr"],["10.ca","10.co","10.tr"],["2.ca","2.co","2.tr"]];
+    var numberOfActions = tableStack.length;
+    
+    
+    var html = "";
+    for (let index = 0; index < tableStack.length; index++) {
+        var htmlHead= "<div class='col'>";
+        var htmlFooter= "</div>";
+        var htmlBody= "";
+        html=html.concat(htmlHead);
+        for (let jndex = 0; jndex < tableStack[index].length; jndex++) {
+            
+            const element = tableStack[index][jndex];
+            var dotindex = element.indexOf(".");
+            var number = element.slice(0,dotindex);
+            var symbol = element.slice(dotindex+1,element.length); 
+            
+            htmlBody=htmlBody.concat("<div class='card' style='display:inline-block'><a class='' href='#'>");
+            htmlBody=htmlBody.concat(number);
+            htmlBody=htmlBody.concat("<br><img style='width:50px' src='img/cards/");
+            htmlBody=htmlBody.concat(symbol);
+            htmlBody=htmlBody.concat(".png'></a></div>");
+        }
+        html=html.concat(htmlBody);
+        html=html.concat(htmlFooter);
+        
+    }
+    console.log(html);
+    
+    return html;
+}
 
 function activateNavItem(){
-    this.classList.toggle('active');
+    onglets.forEach(onglet => onglet.classList.remove("active"));
+    this.classList.toggle("active");
     console.log("clicked");
 }
 onglets.forEach(onglet => onglet.addEventListener('click',activateNavItem));
