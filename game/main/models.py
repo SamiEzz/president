8c8b3c3d26ad1fc5f0a3a29f2474f16dcabba444
Sami Ezzerouali <sami.ezzerouali@hotmail.com>
2021-03-07 00:49:36 +0100

    ui simple working

--- game/main/models.py
+++ game/main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Player(models.Model):
+    player_name = models.CharField(max_length=200)
+    player_score = models.CharField(max_length=200)
+    player_avatar = models.CharField(max_length=300)
+    player_email = models.EmailField()
+    
+    def __str__(self):
+        return self.player_name
+
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=200)
+    room_online_players = []
+    
+    def __str__(self):
+        return self.room_name
--- game/main/views.py
+++ game/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Player
 
 # Create your views here.
+
+def homepage(request):
+    return render(request=request,template_name="main/home.html",context={"mails": Player.objects.all})
